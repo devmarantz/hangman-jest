22c7334654f19b42bfe4a714315123f4fe25baa5
Devon Marantz <DMarantz@talentpath.com>
2019-08-01 16:03:37 -0700

    Write test suite for playHangman

--- hangman.test.js
+++ hangman.test.js
@@ -1,3 +1,36 @@
+const hangman = require('./hangman');
+
+// jest.mock('readline-sync');
 describe('playHangman', () => {
-  it('works', () => {});
+  console.log = jest.fn();
+  const hangmanSpy = jest.spyOn(hangman, 'playHangman');
+
+  afterEach(() => {
+    // resets our mock after each test
+    console.log.mockClear();
+    hangmanSpy.mockClear();
+  });
+
+  it('should print a success message and end when a completed word has been guessed', () => {
+    const targetWord = 'abc';
+    const guessedWord = 'abc'.split('');
+    hangman.playHangman(targetWord, guessedWord);
+    expect(console.log).toBeCalledTimes(1);
+    expect(console.log).toBeCalledWith(`You win! The word was ${targetWord}`);
+    // make sure the recursive function is not ran again
+    expect(hangmanSpy).toBeCalledTimes(1);
+  });
+
+  it('should print a failure message and end when you have run out of strikes', () => {
+    const targetWord = 'abc';
+    const guessedWord = 'a_c'.split('');
+    const maxStrikes = 5;
+    hangman.playHangman(targetWord, guessedWord, maxStrikes, maxStrikes);
+    expect(console.log).toBeCalledTimes(1);
+    expect(console.log).toBeCalledWith(`You lose! The word was ${targetWord}`);
+    // make sure the recursive function is not ran again
+    expect(hangmanSpy).toBeCalledTimes(1);
+  });
+
+  it.todo('should continue play if there was no win or loss');
 });
